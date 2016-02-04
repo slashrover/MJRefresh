897ee5fef1bd6c4a904da923e2ac5edd88ce087c
Hengda.Xiao <hengda.xiao@coach4pro.com>
2016-02-04 13:39:53 +0200

    change chinese to english

--- MJRefresh/MJRefreshConst.m
+++ MJRefresh/MJRefreshConst.m
@@ -14,15 +14,15 @@ NSString *const MJRefreshKeyPathPanState = @"state";
 
 NSString *const MJRefreshHeaderLastUpdatedTimeKey = @"MJRefreshHeaderLastUpdatedTimeKey";
 
-NSString *const MJRefreshHeaderIdleText = @"下拉可以刷新";
-NSString *const MJRefreshHeaderPullingText = @"松开立即刷新";
-NSString *const MJRefreshHeaderRefreshingText = @"正在刷新数据中...";
+NSString *const MJRefreshHeaderIdleText = @"Pull to refresh";
+NSString *const MJRefreshHeaderPullingText = @"Release to refresh";
+NSString *const MJRefreshHeaderRefreshingText = @"Refreshing...";
 
-NSString *const MJRefreshAutoFooterIdleText = @"点击或上拉加载更多";
-NSString *const MJRefreshAutoFooterRefreshingText = @"正在加载更多的数据...";
-NSString *const MJRefreshAutoFooterNoMoreDataText = @"已经全部加载完毕";
+NSString *const MJRefreshAutoFooterIdleText = @"Pull up to load more";
+NSString *const MJRefreshAutoFooterRefreshingText = @"Loading...";
+NSString *const MJRefreshAutoFooterNoMoreDataText = @"Load is done";
 
-NSString *const MJRefreshBackFooterIdleText = @"上拉可以加载更多";
-NSString *const MJRefreshBackFooterPullingText = @"松开立即加载更多";
-NSString *const MJRefreshBackFooterRefreshingText = @"正在加载更多的数据...";
-NSString *const MJRefreshBackFooterNoMoreDataText = @"已经全部加载完毕";
+NSString *const MJRefreshBackFooterIdleText = @"Pull up to load more";
+NSString *const MJRefreshBackFooterPullingText = @"Release to load more";
+NSString *const MJRefreshBackFooterRefreshingText = @"Loading...";
+NSString *const MJRefreshBackFooterNoMoreDataText = @"Load is done";
