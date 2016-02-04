d4a2b0e69c8c7c8181e5c800ffae00f663fecab9
Hengda.Xiao <hengda.xiao@coach4pro.com>
2016-02-04 13:36:19 +0200

    change chinese to english

--- MJRefresh/Custom/Header/MJRefreshStateHeader.m
+++ MJRefresh/Custom/Header/MJRefreshStateHeader.m
@@ -65,40 +65,40 @@
 - (void)setLastUpdatedTimeKey:(NSString *)lastUpdatedTimeKey
 {
     [super setLastUpdatedTimeKey:lastUpdatedTimeKey];
-    
+
     // 如果label隐藏了，就不用再处理
     if (self.lastUpdatedTimeLabel.hidden) return;
-    
+
     NSDate *lastUpdatedTime = [[NSUserDefaults standardUserDefaults] objectForKey:lastUpdatedTimeKey];
-    
+
     // 如果有block
     if (self.lastUpdatedTimeText) {
         self.lastUpdatedTimeLabel.text = self.lastUpdatedTimeText(lastUpdatedTime);
         return;
     }
-    
+
     if (lastUpdatedTime) {
         // 1.获得年月日
         NSCalendar *calendar = [self currentCalendar];
         NSUInteger unitFlags = NSCalendarUnitYear| NSCalendarUnitMonth | NSCalendarUnitDay |NSCalendarUnitHour |NSCalendarUnitMinute;
         NSDateComponents *cmp1 = [calendar components:unitFlags fromDate:lastUpdatedTime];
         NSDateComponents *cmp2 = [calendar components:unitFlags fromDate:[NSDate date]];
-        
+
         // 2.格式化日期
         NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
         if ([cmp1 day] == [cmp2 day]) { // 今天
-            formatter.dateFormat = @"今天 HH:mm";
+            formatter.dateFormat = @"Today HH:mm";
         } else if ([cmp1 year] == [cmp2 year]) { // 今年
             formatter.dateFormat = @"MM-dd HH:mm";
         } else {
             formatter.dateFormat = @"yyyy-MM-dd HH:mm";
         }
         NSString *time = [formatter stringFromDate:lastUpdatedTime];
-        
+
         // 3.显示日期
-        self.lastUpdatedTimeLabel.text = [NSString stringWithFormat:@"最后更新：%@", time];
+        self.lastUpdatedTimeLabel.text = [NSString stringWithFormat:@"Last Update：%@", time];
     } else {
-        self.lastUpdatedTimeLabel.text = @"最后更新：无记录";
+        self.lastUpdatedTimeLabel.text = @"Last Update：No Record";
     }
 }
 
@@ -106,7 +106,7 @@
 - (void)prepare
 {
     [super prepare];
-    
+
     // 初始化文字
     [self setTitle:MJRefreshHeaderIdleText forState:MJRefreshStateIdle];
     [self setTitle:MJRefreshHeaderPullingText forState:MJRefreshStatePulling];
@@ -116,11 +116,11 @@
 - (void)placeSubviews
 {
     [super placeSubviews];
-    
+
     if (self.stateLabel.hidden) return;
-    
+
     BOOL noConstrainsOnStatusLabel = self.stateLabel.constraints.count == 0;
-    
+
     if (self.lastUpdatedTimeLabel.hidden) {
         // 状态
         if (noConstrainsOnStatusLabel) self.stateLabel.frame = self.bounds;
@@ -133,7 +133,7 @@
             self.stateLabel.mj_w = self.mj_w;
             self.stateLabel.mj_h = stateLabelH;
         }
-        
+
         // 更新时间
         if (self.lastUpdatedTimeLabel.constraints.count == 0) {
             self.lastUpdatedTimeLabel.mj_x = 0;
@@ -147,10 +147,10 @@
 - (void)setState:(MJRefreshState)state
 {
     MJRefreshCheckState
-    
+
     // 设置状态文字
     self.stateLabel.text = self.stateTitles[@(state)];
-    
+
     // 重新设置key（重新显示时间）
     self.lastUpdatedTimeKey = self.lastUpdatedTimeKey;
 }
